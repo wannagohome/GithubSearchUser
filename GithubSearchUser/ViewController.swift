5e7167746b19f76887024ea5db9b063c82958ca0
pete <iwwuf7@gmail.com>
2020-10-17 18:07:08 +0900

    Initial Commit

---
+++ GithubSearchUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubSearchUser
+//
+//  Created by Jinho Jang on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
